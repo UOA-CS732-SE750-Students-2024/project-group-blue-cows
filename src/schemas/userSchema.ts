ec3780841ce5f8b89ce22c9612551de882d8666c
Luca Eastwood <77914022+leas022@users.noreply.github.com>
2024-04-20 23:46:18 +1200

    :bricks: add user schema

---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+// The fields will grow as we add additional features
+export default pgTable("users", {
+  id: text("id").primaryKey(), // intention is to use a Firebase user id as the primary key here, however may change depending on auth implementation
+  name: text("name").notNull(),
+  email: text("email").unique().notNull(),
+  upi: text("upi"), // nullable for non-student user use case, may not be unique across organisations
+  student_id: integer("student_id"), // nullable for non-student user use case, may not be unique across organisations
+  year_of_study: integer("year_of_study"), // nullable for non-student user use case
+});
