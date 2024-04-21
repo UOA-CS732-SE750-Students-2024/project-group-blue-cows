8351b8a33917e533e63f4dcdcf524f9c6a2fe529
Luca Eastwood <77914022+leas022@users.noreply.github.com>
2024-04-21 20:45:40 +1200

    :bug: fix ts import issues

--- src/schemas/adminSchema.ts
+++ src/schemas/adminSchema.ts
@@ -1,8 +1,14 @@
-import {pgTable, text, serial } from "drizzle-orm/pg-core";
+import { pgTable, text, serial, integer } from "drizzle-orm/pg-core";
+import clubSchema from "./clubSchema";
+import userSchema from "./userSchema";
 
 // The fields will grow as we add additional features
 export default pgTable("admins", {
   id: serial("id").primaryKey(),
-  club: integer("club").notNull().references(() => clubSchema.id),
-  user: text("user").notNull().references(() => userSchema.id)
+  club: integer("club")
+    .notNull()
+    .references(() => clubSchema.id),
+  user: text("user")
+    .notNull()
+    .references(() => userSchema.id),
 });
--- src/schemas/membershipSchema.ts
+++ src/schemas/membershipSchema.ts
@@ -1,9 +1,15 @@
-import {pgTable, text, serial } from "drizzle-orm/pg-core";
+import { pgTable, text, serial, integer, boolean } from "drizzle-orm/pg-core";
+import clubSchema from "./clubSchema";
+import userSchema from "./userSchema";
 
 // The fields will grow as we add additional features
 export default pgTable("memberships", {
   id: serial("id").primaryKey(),
-  club: integer("club").notNull().references(() => clubSchema.id),
-  user: text("user").notNull().references(() => userSchema.id),
-  paid: boolean("paid").notNull()
+  club: integer("club")
+    .notNull()
+    .references(() => clubSchema.id),
+  user: text("user")
+    .notNull()
+    .references(() => userSchema.id),
+  paid: boolean("paid").notNull(),
 });
