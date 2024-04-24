c2ddeb65992d9bea2d09de75aed5e355b60c19ee
Tristan Mona <tmon261@UoA.auckland.ac.nz>
2024-04-24 17:15:02 +1200

    login bp'd, can't use yarn on uni computer

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+// https://github.com/shadcn-ui/ui/blob/main/apps/www/app/(app)/examples/authentication/components/user-auth-form.tsx
+
+export default function LoginPage() {
+    return (
+      <main>
+        <h1>Clubs</h1>
+      </main>
+    );
+  }
