963f3b7ec1462ac3572b0e65c5759083e12f7989
TrissyG <tmon261@aucklanduni.ac.nz>
2024-04-25 18:43:20 +1200

    :sparkles: auth routing group added, layout for routing group initialised

--- src/app/(auth)/login/components/authform.tsx
+++ src/app/(auth)/login/components/authform.tsx
@@ -1,28 +1,55 @@
-"use client"
+"use client";
 // https://github.com/shadcn-ui/ui/blob/main/apps/www/app/(app)/examples/authentication/components/user-auth-form.tsx
-import { cn } from "@/util/cnUtils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input" 
-import { Label } from "@/components/ui/label"; 
+import { cn } from "@/util/cnUtils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { IoReload } from "react-icons/io5";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import React from "react";
 
 interface UserAuthProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const auth = getAuth();
+const uiConfig = {
+  signInFlow: "popup",
+  signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    // signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+    //   // Handle sign-in success. For example, you can set the user's session and redirect to a different page.
+    //   console.log(authResult, redirectUrl);
+    //   return false; // This tells FirebaseUI not to redirect.
+    // },
+  },
+};
+
 export default function AuthForm({ className, ...props }: UserAuthProps) {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
+    event.preventDefault();
+    setIsLoading(true);
 
     setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
+      setIsLoading(false);
+    }, 3000);
   }
-  
-    return (
-      <div className={cn("grid gap-6", className)} {...props}>
+
+  async function firebaseSignIn() {
+    setIsLoading(true);
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
       <form onSubmit={onSubmit}>
         <div className="grid gap-2">
           <div className="grid gap-1">
@@ -54,13 +81,19 @@ export default function AuthForm({ className, ...props }: UserAuthProps) {
             />
           </div>
           <Button disabled={isLoading}>
-            {isLoading && (
-              <IoReload className="mr-2 h-4 w-4 animate-spin" />
-            )}
+            {isLoading && <IoReload className="mr-2 h-4 w-4 animate-spin" />}
             Sign In with Email
           </Button>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+          <div className={cn("grid gap-6", className)} {...props}>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+          </div>
         </div>
       </form>
-      </div>
-    );
-  }
+    </div>
+  );
+}
