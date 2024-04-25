e68739450e157223fa695fbbb64050178c5970ba
Luca Eastwood <77914022+leas022@users.noreply.github.com>
2024-04-25 23:38:48 +1200

    :bricks: update users schema for our use case

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,14 +1,16 @@
+// This file is based off Auth.js's recommended schemas (they can be found at https://authjs.dev/getting-started/adapters/drizzle)
 import {
   timestamp,
   pgTable,
   text,
   primaryKey,
-  integer,
   index,
+  integer,
 } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "next-auth/adapters";
 import { randomUUID } from "crypto";
 
+// We can add more fields to user as required for additional functionality, it is only important we have everything required for MVP currently
 export const users = pgTable("user", {
   id: text("id")
     .primaryKey()
@@ -16,9 +18,13 @@ export const users = pgTable("user", {
   name: text("name"),
   email: text("email").notNull().unique(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
-  image: text("image"),
+  image: text("image"), // profile picture
+  upi: text("upi"), // UPI, student ID and year of study are optional because they will likely be added after account creation
+  student_id: text("student_id"), // text because it ensures all digits are stored, we don't need to do calculations and do not know an official maximum value
+  year_of_study: integer("year_of_study"),
 });
 
+// Altering these tables could impact authentication/oauth flows, so make changes with care
 export const accounts = pgTable(
   "account",
   {
