c803d3031e4b0558e59d993a809887235d7cc8fa
TrissyG <tmon261@aucklanduni.ac.nz>
2024-04-28 04:54:25 +1200

    :sparkles: added user profile navbar element v0.1

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -14,10 +14,12 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import Link from "next/link";
+import NavProfile from "@/components/layout/nav-profile";
 
 export default function Nav() {
   return (
     <aside className="inset-y-0 w-full flex flex-col border-r bg-blue-custom">
+      <NavProfile />
       <TooltipProvider>
         <nav className="flex flex-col w-full items-center gap-4 px-2 lg:py-10">
           <Tooltip>
