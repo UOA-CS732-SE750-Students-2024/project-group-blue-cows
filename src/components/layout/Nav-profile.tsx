13447c438bbae3d32d4dc357c6cb0254cd4b15ea
TrissyG <tmon261@aucklanduni.ac.nz>
2024-04-28 04:54:25 +1200

    :sparkles: added user profile navbar element v0.1

---
+++ src/components/layout/Nav-profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavProfile() {
+  return (
+    <div className="flex items-center py-10">
+      <img
+        src="/Tristan-Midjourney.png"
+        alt="User Profile"
+        className="rounded-full w-10 h-10 mr-2"
+      />
+      <div>
+        <p className="text-sm font-medium text-white">User Name</p>
+        <button className="text-white hover:text-blue-700">Edit Profile</button>
+      </div>
+    </div>
+  );
+}
