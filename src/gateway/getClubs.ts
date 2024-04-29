f847bba74005faf11caa7031a63d66ab7f0c5739
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-04-30 01:31:55 +1200

    refactor: moved server function to service layer

--- src/gateway/getClubs.ts
+++ src/gateway/getClubs.ts
@@ -1,15 +1,14 @@
-import "server-only";
 import { db } from "../config/db";
 import clubSchema, { Club } from "@/schemas/clubSchema";
 import {sql} from 'drizzle-orm';
 
 export async function getClubs(name:string, filter?: string) {
-    let filterBuilder = sql``
+    let filterBuilder = sql<string>``
     if(filter) {
-        filterBuilder = sql`${clubSchema.category} = ${filter} and ${clubSchema.name} LIKE ${name}%`
+        filterBuilder = sql<string>`${clubSchema.category} = ${filter} and ${clubSchema.name} LIKE ${name}%`
     }    
     else {
-        filterBuilder = sql`${clubSchema.name} LIKE ${name}%`
+        filterBuilder = sql<string>`${clubSchema.name} LIKE ${name}%`
     }
     return await db.select().from(clubSchema).where(filterBuilder) as Club[]
 }
