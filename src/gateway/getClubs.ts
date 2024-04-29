bf5eeb8c474e4211d01cf8e4955276f08df6b6a4
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-04-29 13:05:02 +1200

    refactor: changed number to string for price

--- src/gateway/getClubs.ts
+++ src/gateway/getClubs.ts
@@ -1,9 +1,9 @@
 import "server-only";
 import { db } from "../config/db";
-import clubSchema from "@/schemas/clubSchema";
+import clubSchema, { Club } from "@/schemas/clubSchema";
 import {sql} from 'drizzle-orm';
 
 export async function getClubs(name:string) {
-    return await db.select().from(clubSchema).where(sql`${clubSchema.name} LIKE ${name}%`);
+    return await db.select().from(clubSchema).where(sql`${clubSchema.name} LIKE ${name}%`) as Club[];
 }
 
