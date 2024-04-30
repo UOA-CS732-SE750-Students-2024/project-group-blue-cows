ce0d7a07279f01a2e6e60e1973a4401e05edf499
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-04-30 14:51:03 +1200

    fix: added {

--- src/gateway/getClubs.ts
+++ src/gateway/getClubs.ts
@@ -5,12 +5,12 @@ import clubSchema, { Club } from "@/schemas/clubSchema";
 import {sql} from 'drizzle-orm';
 
 export async function getClubs(name:string, category?: string) {
-    let filterBuilder = sql<string>``
+    let filterBuilder = sql<string>``;
     if(category) {
-        filterBuilder = sql<string>`${clubSchema.category} = ${category} and ${clubSchema.name} LIKE ${name}%`
+        filterBuilder = sql<string>`${clubSchema.category} = ${category} and ${clubSchema.name} LIKE ${name}%`;
     }    
     else {
-        filterBuilder = sql<string>`${clubSchema.name} LIKE ${name}%`
+        filterBuilder = sql<string>`${clubSchema.name} LIKE ${name}%`;
     }
-    return await db.select().from(clubSchema).where(filterBuilder) as Club[]
+    return await db.select().from(clubSchema).where(filterBuilder) as Club[];
 }
