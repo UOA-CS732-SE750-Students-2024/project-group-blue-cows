d68faeef2a4e4bb0162344d05a84eb489a472af4
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-04-30 14:23:58 +1200

    refactor: move component to admin page

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -1,33 +1,9 @@
-"use client";
-import { MembersTable } from '@/components/ui/members-table'
-import { studentData } from '@/gateway/getAllMembersForClub';
-import { getAllMembers } from '@/services/clubServices';
-import React, { useEffect, useState } from 'react'
-import {membersColumns} from "../../components/ui/columns";
-import LoadingSpinner from '@/components/ui/loading-spinner';
+import React from 'react'
 
 const page = () => {
-  const [membersData, setMembersData] = useState<studentData[]>([]);
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    const getData = async() => {
-      const data = await getAllMembers(1);
-      setMembersData(data);
-      setLoading(false)
-    }
-    getData();
-  },[])
-
   return (
-    <div className="flex h-screen">
-      <div>
-        {loading ? <LoadingSpinner /> : <div className="flex justify-center items-center w-screen pl-40 pr-40 mt-10">
-        <MembersTable columns={membersColumns} data={membersData} />
-        </div>}
-      </div>
-    </div>
+    <div>page</div>
   )
 }
-
 
 export default page
