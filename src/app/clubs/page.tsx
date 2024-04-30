cfb6c9eaa41d4d08e49d84d612d4272cec925c6d
Oculux <nate.williamson.nz@gmail.com>
2024-05-01 00:35:19 +1200

    :lipstick: style search page

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -59,9 +59,9 @@ function ClubsSearch({
   setFilter: (arg0: string | null) => void;
 }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 h-1/3 bg-[url('cow-banner.svg')] bg-cover">
-      <h2 className="text-center text-2xl font-semibold">Browse Clubs</h2>
-      <div role="doc-subtitle" className="text-center">
+    <div className="flex flex-col justify-center items-center gap-5 h-[30%] bg-[url('cow-banner.svg')] bg-cover">
+      <h2 className="text-center text-4xl font-semibold">Browse Clubs</h2>
+      <div role="doc-subtitle" className="text-center text-lg">
         Find a club that suits your interests and goals.
       </div>
       <div className="w-1/2 flex relative">
@@ -70,12 +70,12 @@ function ClubsSearch({
           alt=""
           width={5}
           height={5}
-          className="brightness-75 w-5 h-auto absolute top-2.5 left-2.5"
+          className="brightness-75 w-6 h-auto absolute top-3 left-3.5"
         />
         <Input
           type="search"
           placeholder="Search"
-          className="w-full rounded-lg pl-10"
+          className="w-full h-12 rounded-lg pl-12 text-lg"
           value={searchString}
           onChange={(event) => setSearchString(event.target.value)}
         />
@@ -87,27 +87,36 @@ function ClubsSearch({
 
 function ClubsList({ clubs }: { clubs: Club[] }) {
   return (
-    <div className="max-w-full min-w-[50%] m-auto">
-      <h2 className="text-2xl font-semibold">Results</h2>
-      <div role="doc-subtitle" className="">
+    <div className="w-2/3 m-auto">
+      <h2 className="text-4xl font-semibold mt-10">Results</h2>
+      <div role="doc-subtitle" className="text-md">
         Displaying {clubs.length} results
       </div>
       {clubs.map(({ name, logo, description, category, id }) => (
-        <Card key={id} className="min-w-96">
-          <CardContent className="flex">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width={30}
-              height={30}
-              alt=""
-              src={logo}
-              className="w-30 h-auto"
-            />
-            <div>
-              <div role="doc-subtitle">Type</div>
-              <h2>Title</h2>
-              <p>Text</p>
+        <Card key={id} className="min-w-96 mt-5 shadow-sm shadow-slate-500">
+          <CardContent className="px-6 py-4">
+            <div className="flex gap-6">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                alt={`${name} logo`}
+                src={logo}
+                className="w-24 h-24 rounded-2xl mt-6"
+              />
+              <div className="overflow-hidden">
+                <div
+                  role="doc-subtitle"
+                  className="text-md mb-3 line-clamp-1 text-ellipsis"
+                >
+                  {category}
+                </div>
+                <h3 className="text-3xl font-semibold text-wrap break-word line-clamp-2">
+                  {name}
+                </h3>
+              </div>
             </div>
+            <p className="mt-6 text-md text-wrap break-word line-clamp-4">
+              {description}
+            </p>
           </CardContent>
         </Card>
       ))}
