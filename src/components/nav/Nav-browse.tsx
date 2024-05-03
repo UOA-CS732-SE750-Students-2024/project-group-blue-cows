7f8f0d29ec19f2691b0d05f5d30877522f8d6d47
Oculux <nate.williamson.nz@gmail.com>
2024-05-03 13:27:40 +1200

    :art: make generic NavTab component

--- src/components/nav/Nav-browse.tsx
+++ src/components/nav/Nav-browse.tsx
@@ -2,23 +2,15 @@ import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import NavTab from "./NavTab";
 
 export default function NavBrowse() {
   return (
     <div className="flex flex-col items-start py-2">
       <h1 className="text-white text-sm">BROWSE CLUBS</h1>
-      <Link href="/clubs">
-        <Button className="bg-blue-custom px-0 hover:bg-transparent">
-          <Image
-            src="nav-search-icon.svg"
-            width={20}
-            height={20}
-            alt="Search Icon"
-            className="icon mr-2 left-0"
-          />
-          <span className="text">Search For Clubs</span>
-        </Button>
-      </Link>
+      <NavTab href="/clubs" imgSrc="nav-search-icon.svg" imgAlt="Search Icon">
+        Search For Clubs
+      </NavTab>
     </div>
   );
 }
