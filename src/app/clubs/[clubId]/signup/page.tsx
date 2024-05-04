5e5a0592e9cbe5282e84d43d3c801ff0c694dc38
TrissyG <tmon261@aucklanduni.ac.nz>
2024-05-04 15:43:02 +1200

    :lipstick: initialise ClubMembershipForm

--- src/app/clubs/[clubId]/signup/page.tsx
+++ src/app/clubs/[clubId]/signup/page.tsx
@@ -1,7 +1,49 @@
-export default function SignupPage({ params }: { params: { clubId: string } }) {
+"use client"; // to get react to know it's a client compponent
+
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { postClub } from "@/services/clubServices";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  Select,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormControl,
+} from "@/components/ui/form";
+
+import * as z from "zod";
+import FormWrapper from "@/components/ui/form-wrapper";
+import ClubMembershipForm from "@/components/ui/ClubMembershipForm";
+
+export default function Page() {
   return (
-    <main>
-      <h1>Signup For Club ID: {params.clubId}</h1>
-    </main>
+    <section className="w-full">
+      <div className="h-screen justify-center w-full pt-20 ">
+        <FormWrapper label="Membership" title="Join _____">
+          <ClubMembershipForm />
+        </FormWrapper>
+      </div>
+    </section>
   );
 }
