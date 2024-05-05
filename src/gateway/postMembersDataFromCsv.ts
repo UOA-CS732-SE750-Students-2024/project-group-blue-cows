4ca83bc3c14bcb518c1fa0ed2b5187e795d88e3d
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-05 21:19:59 +1200

    refactor: refactored csvUtils method

--- src/gateway/postMembersDataFromCsv.ts
+++ src/gateway/postMembersDataFromCsv.ts
@@ -2,8 +2,9 @@ import { importCsvFile } from "@/util/csvUtils";
 
 export async function postMembersDataFromCSV(clubId: number, formData: FormData) {
     //Need to get the JSON structure from the CSV function
-    const values = await importCsvFile(formData);
-    console.log(values)
-
+    const studentData = await importCsvFile(formData);
+    console.log('Extracted values:', studentData);
+    return studentData;
+    
     //With these values, we can then make insert Many to the database
 }
--- src/util/csvUtils.ts
+++ src/util/csvUtils.ts
@@ -4,25 +4,35 @@ import csvParser from 'csv-parser';
 import { studentData } from '@/gateway/getAllMembersForClub';
 import { revalidatePath } from 'next/cache';
 
-export const importCsvFile = async(formData: FormData) => {
-    const file = formData.get("file") as File;
-    const arrayBuffer = await file.arrayBuffer();
-    const buffer = new Uint8Array(arrayBuffer);
-    await fs.writeFile(`./public/uploads/${file.name}`, buffer);
-    const extractedValues: studentData[] = [];
-    originalFS.createReadStream(`./public/uploads/${file.name}`)
-    .pipe(csvParser())
-    .on('data', (row) => {
-        extractedValues.push(row);
-    })
-    .on('end', () => {
-        console.log('Extracted values:', extractedValues);
-        revalidatePath("/");
-        return extractedValues;
-    });
-    
-    return extractedValues;
-}
+export const importCsvFile = async (formData: FormData) => {
+    try {
+        const file = formData.get("file") as File;
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = new Uint8Array(arrayBuffer);
+        await fs.writeFile(`./public/uploads/${file.name}`, buffer);
+
+        return new Promise<studentData[]>((resolve, reject) => {
+            const extractedValues: studentData[] = [];
+            originalFS.createReadStream(`./public/uploads/${file.name}`)
+                .pipe(csvParser())
+                .on('data', (row) => {
+                    extractedValues.push(row);
+                })
+                .on('end', () => {
+                    console.log('Extracted values:', extractedValues);
+                    revalidatePath("/");
+                    resolve(extractedValues as studentData[]);
+                })
+                .on('error', (error) => {
+                    reject(error);
+                });
+        });
+    } catch (error) {
+        console.error('Error importing CSV file:', error);
+        throw new Error('Error importing CSV file');
+    }
+};
+
 
 export const exportCsvFile = (data: studentData[]) => {
     const csv = data.map(row => Object.values(row).join(',')).join('\n');
