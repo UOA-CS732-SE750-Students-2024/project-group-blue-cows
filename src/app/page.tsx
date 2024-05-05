a34c5f5ab2aeb543ccb7fcbc9a9faa03101668ab
Oculux <nate.williamson.nz@gmail.com>
2024-05-05 18:14:32 +1200

    :zap: move redirect to server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-"use client";
-
-import { AppUser } from "@/schemas/authSchema";
-import { useSession } from "next-auth/react";
+import { auth } from "@/util/auth";
 import { redirect } from "next/navigation";
 
 // Testing page is now at /test
-export default function RootPage() {
-  const session = useSession();
-  const user = session?.data?.user as AppUser;
-  redirect(user ? "/users/me/clubs" : "/clubs");
+export default async function RootPage() {
+  const session = await auth();
+  redirect(session ? "/users/me/clubs" : "/clubs");
 }
