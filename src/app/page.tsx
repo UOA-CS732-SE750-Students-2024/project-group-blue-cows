d82193d1d5436feb7f3f9d1153a4367c1a558d10
Oculux <nate.williamson.nz@gmail.com>
2024-05-05 12:48:50 +1200

    :sparkles: redirect to /users/me/clubs if user is logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { AppUser } from "@/schemas/authSchema";
+import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 // Testing page is now at /test
 export default function RootPage() {
-  redirect("/clubs");
+  const session = useSession();
+  const user = session?.data?.user as AppUser;
+  redirect(user ? "/users/me/clubs" : "/clubs");
 }
