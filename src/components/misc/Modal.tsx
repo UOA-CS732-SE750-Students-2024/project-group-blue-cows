7b065de2cadd320d9d8f80a86979d70464f1bee8
Oculux <nate.williamson.nz@gmail.com>
2024-05-05 16:25:17 +1200

    temp

--- src/components/misc/Modal.tsx
+++ src/components/misc/Modal.tsx
@@ -1,15 +1,17 @@
+"use client";
 import {
   Dialog,
   DialogTrigger,
   DialogContent,
   DialogTitle,
 } from "@radix-ui/react-dialog";
-import { signIn } from "next-auth/react";
-import { toast } from "react-toastify";
 import { ButtonProps, Button } from "../ui/button";
-import { DialogHeader } from "../ui/dialog";
+import { useState } from "react";
+import { showToastDemo } from "@/util/toastUtils";
 
 export function Modal({ children, className, ...props }: ButtonProps) {
+  const [open, setOpen] = useState(false);
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -18,28 +20,16 @@ export function Modal({ children, className, ...props }: ButtonProps) {
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle className="text-center text-2xl">
-            Log In / Register
-          </DialogTitle>
-        </DialogHeader>
+        <DialogTitle className="text-center text-2xl">
+          Log In / Register
+        </DialogTitle>
         <Button
           type="submit"
           className="flex items-center justify-center gap-2 bg-white text-gray-700 border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-50 text-xl h-24 w-full"
           onClick={() => {
-            toast.info("Loading...", {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-            });
-            signIn("google");
+            showToastDemo("Sign In with Google");
           }}
         >
-          <img src="/google.svg" alt="Google Logo" className="w-8 h-8" />
           Sign In with Google
         </Button>
       </DialogContent>
