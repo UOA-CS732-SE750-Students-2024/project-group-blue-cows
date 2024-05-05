c83d1c61b8885bb67b3899459c19f2fef1fc27bd
Oculux <nate.williamson.nz@gmail.com>
2024-05-05 16:15:33 +1200

    :lipstick: animate back button

--- src/components/misc/buttons.tsx
+++ src/components/misc/buttons.tsx
@@ -22,14 +22,15 @@ export function BlueButton({ children, className, ...props }: ButtonProps) {
   );
 }
 
-export function BackButton({ children, className, ...props }: ButtonProps) {
+export function BackButton({ className, ...props }: ButtonProps) {
   return (
     <Button
-      className={`bg-customAccent hover:bg-customAccent hover:brightness-90 h-16 w-16 rounded-full hover:pr-6 ${className}`}
+      className={`bg-customAccent hover:bg-customAccent hover:brightness-90 h-16 w-16 p-0 rounded-full ${className}`}
       {...props}
     >
-      <div className="w-0 h-0 border-transparent border-r-black border-y-[1em] border-r-[1.2em] relative left-[-0.1em]" />
-      {children}
+      <div className="w-full h-full flex justify-center items-center relative left-[-0.2em] transition-transform hover:-translate-x-1">
+        <div className="w-0 h-0 border-transparent border-r-black border-y-[1em] border-r-[1.2em]" />
+      </div>
     </Button>
   );
 }
