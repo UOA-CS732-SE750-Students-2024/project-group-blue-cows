25612af067c7b1af336f0d427d99a384515f279d
vdav02 <100258989+vdav02@users.noreply.github.com>
2024-05-06 22:17:16 +1200

    Small layout fixes
    Adjusted spacing between columns and text.

--- src/app/clubs/[clubId]/view/page.tsx
+++ src/app/clubs/[clubId]/view/page.tsx
@@ -35,85 +35,84 @@ export default function ClubViewPage({ params }: { params: { clubId: string } })
         <LoadingSpinner />
       ) : (
         <div className="w-full">
-        <div className="relative h-96 rounded-b flex justify-start w-full">
-          <img
-            src="https://picsum.photos/id/1018/3000"
-            className="object-cover w-full h-full rounded-b"
-            alt="cover"
-          />
-          <div className="absolute -bottom-20 left-20">
+          <div className="relative h-96 rounded-b flex justify-start w-full">
             <img
-              src={clubData?.logo}
-              className="object-cover border-8 border-white w-40 h-40 rounded-md"
-              alt="club logo"
+              src="https://picsum.photos/id/1018/3000"
+              className="object-cover w-full h-full rounded-b"
+              alt="cover"
             />
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-2 p-5">
-            <div className="flex flex-col space-y-4 mt-20 ml-16 ">
-              {/* Social Links Section */}
-              <div><a href="#">Facebook</a></div>
-              <div><a href="#">Twitter</a></div>
-              <div><a href="#">LinkedIn</a></div>
-              <div><a href="#">Other Socials</a></div>
+            <div className="absolute -bottom-20 left-20">
+              <img
+                src={clubData?.logo}
+                className="object-cover border-8 border-white w-40 h-40 rounded-md"
+                alt="club logo"
+              />
             </div>
-            <div>
+          </div>
+          <div className="grid grid-cols-5 gap-1 p-5">
+            <div className="flex flex-col space-y-4 mt-20 ml-16 col-span-1">
+              {/* Social Links Section */}
+              <div>
+                <a href="#">Facebook</a>
+              </div>
+              <div>
+                <a href="#">Twitter</a>
+              </div>
+              <div>
+                <a href="#">LinkedIn</a>
+              </div>
+              <div>
+                <a href="#">Other Socials</a>
+              </div>
+            </div>
+            <div className="col-span-4 mr-20">
               {/* Title, Description and Gallery Section */}
-              <h2 className="text-lg font-bold">{clubData?.name}</h2>
-              <p>{clubData?.description}</p>
+              <div>
+                <h1 className="text-lg font-bold pb-6">{clubData?.name}</h1>
+                <h2 className="text-md font-semibold pb-2">DESCRIPTION</h2>
+                <p>{clubData?.description}</p>
+              </div>
               {/* Gallery can be implemented here */}
             </div>
-      </div>
-      </div>
-      
+          </div>
+        </div>
 
-      
+        //   //div below is equivalent to header_wrapper
+        //   <div>
+        //     <header></header>
+        //     {/* div below is equivalent to cols_container */}
+        //     <div>
+        //       {/* div below is equivalent to left_col */}
+        //       <div className='px-[20px] py-[25px] text-center max-w-[350px] relative mx-auto  '>
+        //         {/* div below is equivalent to img_container */}
+        //         <div className='absolute -top-[60px] left-2/4 [transform:translatex(-50%)]'>
+        //           <img src={clubData?.logo} alt="club logo" />
+        //           <span></span>
+        //         </div>
+        //         <h2>{clubData?.name}</h2>
+        //         {/* <p>{clubData?.aliases</p> JUST MOVE THIS LINE WHEREVER THE NAME IS FOR NOW, IT'LL GO UNDER THE NAME IF WE CHOOSE TO ACTUALLY DO THIS */}
+        //         <p>Socials</p>
+        //         <p>Social Media Data here once its ready to go</p>
+        //       </div>
 
+        //       {/* div below is equivalent to right_col */}
+        //       <div>
+        //         <nav>
+        //           <button>Follow</button>
+        //         </nav>
 
+        //         <div>
+        //           <img src={clubData?.logo} alt="club logo" />
+        //           <img src={clubData?.logo} alt="club logo" />
+        //           <img src={clubData?.logo} alt="club logo" />
+        //           <img src={clubData?.logo} alt="club logo" />
+        //           <img src={clubData?.logo} alt="club logo" />
 
-
-
-
-
-
-
-
-      //   //div below is equivalent to header_wrapper
-      //   <div>
-      //     <header></header>
-      //     {/* div below is equivalent to cols_container */}
-      //     <div>
-      //       {/* div below is equivalent to left_col */}
-      //       <div className='px-[20px] py-[25px] text-center max-w-[350px] relative mx-auto  '>
-      //         {/* div below is equivalent to img_container */}
-      //         <div className='absolute -top-[60px] left-2/4 [transform:translatex(-50%)]'>
-      //           <img src={clubData?.logo} alt="club logo" />
-      //           <span></span>
-      //         </div>
-      //         <h2>{clubData?.name}</h2>
-      //         {/* <p>{clubData?.aliases</p> JUST MOVE THIS LINE WHEREVER THE NAME IS FOR NOW, IT'LL GO UNDER THE NAME IF WE CHOOSE TO ACTUALLY DO THIS */}
-      //         <p>Socials</p>
-      //         <p>Social Media Data here once its ready to go</p>
-      //       </div>
-
-      //       {/* div below is equivalent to right_col */}
-      //       <div>
-      //         <nav>
-      //           <button>Follow</button>
-      //         </nav>
-
-      //         <div>
-      //           <img src={clubData?.logo} alt="club logo" />
-      //           <img src={clubData?.logo} alt="club logo" />
-      //           <img src={clubData?.logo} alt="club logo" />
-      //           <img src={clubData?.logo} alt="club logo" />
-      //           <img src={clubData?.logo} alt="club logo" />
-
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // )}
+        //         </div>
+        //       </div>
+        //     </div>
+        //   </div>
+        // )}
       )}
     </div>
   );
