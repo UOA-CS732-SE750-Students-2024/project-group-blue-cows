bc5297dd6b44cdeb417c2ab8ad53b5710021aa40
vdav02 <100258989+vdav02@users.noreply.github.com>
2024-05-06 16:41:40 +1200

    Resetting cover image section
    Trying new layout for cover image section

--- src/app/clubs/[clubId]/view/page.tsx
+++ src/app/clubs/[clubId]/view/page.tsx
@@ -6,6 +6,7 @@ import LoadingSpinner from '@/components/ui/loading-spinner';
 import { Club } from "@/schemas/clubSchema";
 import Custom404 from '@/pages/404';
 import Image from 'next/image';
+import TimelineHeader from "@/components/misc/timelineheader";
 
 // Component definition accepting clubId as a prop
 export default function ClubViewPage({ params }: { params: { clubId: string } }) {
@@ -33,41 +34,68 @@ export default function ClubViewPage({ params }: { params: { clubId: string } })
       {loading ? (
         <LoadingSpinner />
       ) : (
-        //div below is equivalent to header_wrapper
-        <div>
-          <header></header>
-          {/* div below is equivalent to cols_container */}
-          <div>
-            {/* div below is equivalent to left_col */}
-            <div className='px-[20px] py-[25px] text-center max-w-[350px] relative mx-auto  '>
-              {/* div below is equivalent to img_container */}
-              <div className='absolute -top-[60px] left-2/4 [transform:translatex(-50%)]'>
-                <img src={clubData?.logo} alt="club logo" />
-                <span></span>
-              </div>
-              <h2>{clubData?.name}</h2>
-              {/* <p>{clubData?.aliases</p> JUST MOVE THIS LINE WHEREVER THE NAME IS FOR NOW, IT'LL GO UNDER THE NAME IF WE CHOOSE TO ACTUALLY DO THIS */}
-              <p>Socials</p>
-              <p>Social Media Data here once its ready to go</p>
-            </div>
-
-            {/* div below is equivalent to right_col */}
-            <div>
-              <nav>
-                <button>Follow</button>
-              </nav>
-
-              <div>
-                <img src={clubData?.logo} alt="club logo" />
-                <img src={clubData?.logo} alt="club logo" />
-                <img src={clubData?.logo} alt="club logo" />
-                <img src={clubData?.logo} alt="club logo" />
-                <img src={clubData?.logo} alt="club logo" />
-
-              </div>
-            </div>
+        <div className="px-44 shadow">
+        <div className="relative h-96 rounded-b flex justify-center w-full">
+          <img
+            src="https://picsum.photos/id/1018/3000"
+            className="object-cover w-full h-full rounded-b"
+            alt="cover"
+          />
+          <div className="absolute -bottom-6">
+            <img
+              src={clubData?.logo}
+              className="object-cover border-4 border-white w-40 h-40"
+              alt="club logo"
+            />
           </div>
         </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+      //   //div below is equivalent to header_wrapper
+      //   <div>
+      //     <header></header>
+      //     {/* div below is equivalent to cols_container */}
+      //     <div>
+      //       {/* div below is equivalent to left_col */}
+      //       <div className='px-[20px] py-[25px] text-center max-w-[350px] relative mx-auto  '>
+      //         {/* div below is equivalent to img_container */}
+      //         <div className='absolute -top-[60px] left-2/4 [transform:translatex(-50%)]'>
+      //           <img src={clubData?.logo} alt="club logo" />
+      //           <span></span>
+      //         </div>
+      //         <h2>{clubData?.name}</h2>
+      //         {/* <p>{clubData?.aliases</p> JUST MOVE THIS LINE WHEREVER THE NAME IS FOR NOW, IT'LL GO UNDER THE NAME IF WE CHOOSE TO ACTUALLY DO THIS */}
+      //         <p>Socials</p>
+      //         <p>Social Media Data here once its ready to go</p>
+      //       </div>
+
+      //       {/* div below is equivalent to right_col */}
+      //       <div>
+      //         <nav>
+      //           <button>Follow</button>
+      //         </nav>
+
+      //         <div>
+      //           <img src={clubData?.logo} alt="club logo" />
+      //           <img src={clubData?.logo} alt="club logo" />
+      //           <img src={clubData?.logo} alt="club logo" />
+      //           <img src={clubData?.logo} alt="club logo" />
+      //           <img src={clubData?.logo} alt="club logo" />
+
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // )}
       )}
     </div>
   );
