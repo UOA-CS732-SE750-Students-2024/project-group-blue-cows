f9fd6b849554e22595736d8acf365cd121ba931a
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-06 12:21:19 +1200

    chore: removed button from test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { getAllMembers } from "@/services/clubServices";
-
-  const handleData = async() => {
-    const values = await getAllMembers(2);
-    console.log(values)
-  }
 
 const page = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-    <Button className="ml-10" onClick={handleData}>
-      Test
-    </Button>
-  </div>
-  )
-}
+      <Button className="ml-10">Test</Button>
+    </div>
+  );
+};
 
-export default page
+export default page;
