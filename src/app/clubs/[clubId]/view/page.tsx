36ab25b30d607718de72471aa250be4f922782a2
vdav02 <100258989+vdav02@users.noreply.github.com>
2024-05-06 15:16:53 +1200

    Setting up core layout of page
    Syntax errors due to the name property used in the HTML tags, which isn't a real property (placeholder for time being)

--- src/app/clubs/[clubId]/view/page.tsx
+++ src/app/clubs/[clubId]/view/page.tsx
@@ -8,8 +8,8 @@ import Custom404 from '@/pages/404';
 
 // Component definition accepting clubId as a prop
 export default function ClubViewPage({ params }: { params: { clubId: string } }) {
-    const [clubData, setClubData] = useState<Club | null>(null);
-      const [loading, setLoading] = useState(true);
+  const [clubData, setClubData] = useState<Club | null>(null);
+  const [loading, setLoading] = useState(true);
 
   // Effect to fetch club data using the provided clubId
   useEffect(() => {
@@ -27,13 +27,35 @@ export default function ClubViewPage({ params }: { params: { clubId: string } })
   }
 
   return (
+    // top most div (below) is equivalent to body
     <div className="flex h-screen overflow-auto">
       {loading ? (
         <LoadingSpinner />
       ) : (
-        <div className="flex flex-col justify-center items-center w-screen px-10">
-          <h1>{clubData.name}</h1>
-          <p>{clubData.description}</p>
+        <div name="header_wrapper">
+          <header></header>
+          <div name="cols_container">
+            <div name="left_col">
+              <div name="img_container">
+                <img src={clubData?.logo} alt="club logo" />
+                <span></span>
+              </div>
+              <h2>{clubData?.name}</h2>
+              {/* <p>{clubData?.aliases</p> JUST MOVE THIS LINE WHEREVER THE NAME IS FOR NOW, IT'LL GO UNDER THE NAME IF WE CHOOSE TO ACTUALLY DO THIS */}
+              <p>Socials</p>
+              <p>Social Media Data here once its ready to go</p>
+            </div>
+
+            <div name="right_col">
+              <nav>
+                <button>Follow</button>
+              </nav>
+
+              <div>
+                <img src"{clubData?.logo}" alt="club logo" />
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
