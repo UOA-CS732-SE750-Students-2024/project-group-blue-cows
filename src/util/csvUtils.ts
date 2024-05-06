c76da733a22240d55f4d36106f1738650bb1bf80
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-06 13:41:13 +1200

    feat: changed to data dir instead of public dir

--- src/util/csvUtils.ts
+++ src/util/csvUtils.ts
@@ -3,35 +3,47 @@ import * as originalFS from "fs";
 import csvParser from 'csv-parser';
 import { studentData } from '@/gateway/getAllMembersForClub';
 import { revalidatePath } from 'next/cache';
+import path from 'path';
 
 export interface studentAllData extends studentData {
     paid: boolean,
     isAdmin: boolean
 }
 
+export const parseCsvFile = async(filename: string) => {
+    return new Promise<studentAllData[]>((resolve, reject) => {
+        const extractedValues: studentAllData[] = [];
+        originalFS.createReadStream(filename)
+            .pipe(csvParser())
+            .on('data', (row) => {
+                extractedValues.push(row);
+            })
+            .on('end', () => {
+                console.log('Extracted values:', extractedValues);
+                revalidatePath("/");
+                resolve(extractedValues as studentAllData[]);
+            })
+            .on('error', (error) => {
+                reject(error);
+            });
+    });
+}
+
 export const importCsvFile = async (formData: FormData) => {
+    
+    const dataDir = path.join(process.cwd(), "./data");
     try {
         const file = formData.get("file") as File;
         const arrayBuffer = await file.arrayBuffer();
-        const buffer = new Uint8Array(arrayBuffer);
-        await fs.writeFile(`./public/uploads/${file.name}`, buffer);
+        const buffer = Buffer.from(arrayBuffer);
 
-        return new Promise<studentAllData[]>((resolve, reject) => {
-            const extractedValues: studentAllData[] = [];
-            originalFS.createReadStream(`./public/uploads/${file.name}`)
-                .pipe(csvParser())
-                .on('data', (row) => {
-                    extractedValues.push(row);
-                })
-                .on('end', () => {
-                    console.log('Extracted values:', extractedValues);
-                    revalidatePath("/");
-                    resolve(extractedValues as studentAllData[]);
-                })
-                .on('error', (error) => {
-                    reject(error);
-                });
-        });
+        await fs.mkdir(dataDir, { recursive: true });
+        const filePath = path.join(dataDir, file.name);
+        await fs.writeFile(filePath, buffer);
+        const extractedValues = await parseCsvFile(filePath);
+        await fs.unlink(filePath);
+        return extractedValues;
+
     } catch (error) {
         console.error('Error importing CSV file:', error);
         throw new Error('Error importing CSV file');
