91a9c3f941e96f260577b931a3c011c428439f6a
Oculux <nate.williamson.nz@gmail.com>
2024-05-07 10:36:25 +1200

    :art: make getAllExtendedFields API on backend sort automatically

--- src/app/clubs/[clubId]/admin/registration/page.tsx
+++ src/app/clubs/[clubId]/admin/registration/page.tsx
@@ -1,12 +1,13 @@
-"use client";
 import EditClubRegistrationForm from "@/components/form/club-edit-registration";
+import { getAllExtendedFields } from "@/services/optionsFormServices";
 
-export default function AdminEditPage({
-  params,
+export default async function AdminEditPage({
+  params: { clubId },
 }: {
   params: { clubId: string };
 }) {
-  console.log(params);
+  const extendedFields = await getAllExtendedFields(+clubId);
+  console.log(extendedFields);
 
   return (
     <div className="h-[calc(100vh-4rem)] w-full p-10">
