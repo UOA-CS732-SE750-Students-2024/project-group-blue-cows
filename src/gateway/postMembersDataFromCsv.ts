078a5e8077a57410516fbb5268e5f7a62ea9385c
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-07 12:37:05 +1200

    continue with experiment

--- src/gateway/postMembersDataFromCsv.ts
+++ src/gateway/postMembersDataFromCsv.ts
@@ -6,7 +6,27 @@ import {postUser} from "./postUser";
 import { getMemberForClub } from "./getMemberForClub";
 
 export async function postMembersDataFromCSV(clubId: number, formData: FormData) {
-    const studentData = await importCsvFile(formData);
+    //const studentData = await importCsvFile(formData);
+    const studentData = [
+        {
+          name: 'Lucy Zhu',
+          email: 'lzhu613@aucklanduni.ac.nz',
+          upi: 'abc',
+          year_of_study: 4,
+          student_id: 'abc',
+          paid: true,
+          isAdmin: false
+        },
+        {
+          name: 'Nate Williamson',
+          email: 'nwil508@aucklanduni.ac.nz',
+          upi: 'def',
+          year_of_study: 3,
+          student_id: 'def',
+          paid: false,
+          isAdmin: false
+        }
+    ]
     studentData.map(async (data) => {
         const user = await getUserByEmail(data.email);
         let id = user?.id
