d158d4cf3aa106b06514d92e5cf018a363af6abb
Oculux <nate.williamson.nz@gmail.com>
2024-05-07 19:37:31 +1200

    :sparkles: implement moveUp, moveDown, and delete

--- src/components/form/club-edit-registration.tsx
+++ src/components/form/club-edit-registration.tsx
@@ -26,13 +26,15 @@ import { useRegistrationEditContext } from "@/components/form/RegistratonEditCon
 
 const formSchema = z.object({});
 
-export default function EditClubRegistrationForm({
-  clubId,
-  initialExtendedFields,
-}: {
-  clubId: number;
-  initialExtendedFields: GetExtendedFormFieldDto[];
-}) {
+interface EditFieldFunctions {
+  readonly addField: () => void;
+  readonly changeField: (index: number, field: GetExtendedFormFieldDto) => void;
+  readonly deleteField: (index: number) => void;
+  readonly moveFieldUp: (index: number) => void;
+  readonly moveFieldDown: (index: number) => void;
+}
+
+export default function EditClubRegistrationForm() {
   const { extendedFields, setExtendedFields } = useRegistrationEditContext();
   console.log(extendedFields);
 
@@ -55,6 +57,41 @@ export default function EditClubRegistrationForm({
     ]);
   }
 
+  function deleteField(index: number) {
+    setExtendedFields([
+      ...extendedFields.slice(0, index),
+      ...extendedFields.slice(index + 1),
+    ]);
+  }
+
+  function moveFieldUp(index: number) {
+    if (index === 0) return;
+    const newFields = [...extendedFields];
+    [newFields[index], newFields[index - 1]] = [
+      newFields[index - 1],
+      newFields[index],
+    ];
+    setExtendedFields(newFields);
+  }
+
+  function moveFieldDown(index: number) {
+    if (index === extendedFields.length - 1) return;
+    const newFields = [...extendedFields];
+    [newFields[index], newFields[index + 1]] = [
+      newFields[index + 1],
+      newFields[index],
+    ];
+    setExtendedFields(newFields);
+  }
+
+  const editFieldFunctions = {
+    addField,
+    changeField,
+    deleteField,
+    moveFieldUp,
+    moveFieldDown,
+  } as const;
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {},
@@ -73,7 +110,7 @@ export default function EditClubRegistrationForm({
               index={index}
               form={form}
               field={extendedFields[index]}
-              changeField={changeField}
+              editFieldFunctions={editFieldFunctions}
             />
           ))}
           <BlueButton type="submit" className="w-full">
@@ -89,13 +126,15 @@ function Field({
   form,
   index,
   field,
-  changeField,
+  editFieldFunctions,
 }: {
   form: UseFormReturn<z.infer<typeof formSchema> | any>;
   index: number;
   field: GetExtendedFormFieldDto;
-  changeField: (index: number, field: GetExtendedFormFieldDto) => void;
+  editFieldFunctions: EditFieldFunctions;
 }) {
+  const { changeField } = editFieldFunctions;
+
   return (
     <Card className="w-full bg-customLight relative">
       <CardContent className="pt-4">
@@ -153,7 +192,10 @@ function Field({
                       <SelectItem value="long">Long Answer</SelectItem>
                     </SelectContent>
                   </Select>
-                  <EditButtons />
+                  <EditButtons
+                    index={index}
+                    editFieldFunctions={editFieldFunctions}
+                  />
                 </div>
                 <FormMessage />
               </FormItem>
@@ -165,12 +207,27 @@ function Field({
   );
 }
 
-function EditButtons() {
+function EditButtons({
+  index,
+  editFieldFunctions,
+}: {
+  index: number;
+  editFieldFunctions: EditFieldFunctions;
+}) {
+  const { moveFieldUp, moveFieldDown, deleteField } = editFieldFunctions;
+
   return (
     <div className="absolute top-3 right-6 flex">
-      <MiniArrowButton className="rotate-90" />
-      <MiniArrowButton className="-rotate-90" />
+      <MiniArrowButton
+        onClick={() => moveFieldUp(index)}
+        className="rotate-90"
+      />
+      <MiniArrowButton
+        onClick={() => moveFieldDown(index)}
+        className="-rotate-90"
+      />
       <MiniIconButton
+        onClick={() => deleteField(index)}
         icon="/delete.svg"
         alt="delete"
         className="hover:bg-red-200"
