bfb5ab775403256fbd4b16e1d90f067bca8821df
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-08 01:55:51 +1200

    fix: prod bug with persistence

--- src/gateway/postMembersData.ts
+++ src/gateway/postMembersData.ts
@@ -6,35 +6,32 @@ import { getUserByEmail } from "./user/getUserByEmail";
 import { postUser } from "./user/postUser";
 
 export async function postMembersData(clubId: number, memberData: studentAllData[]) {
-    memberData.map(async (data) => {
+  for(const data of memberData) {
     const user = await getUserByEmail(data.email);
-    let id = user?.id;
-    if (!user) {
-      const newId = await postUser({
-        name: data.name,
-        email: data.email,
-        upi: data.upi,
-        year_of_study: data.year_of_study,
-        student_id: data.student_id,
-      });
-      if (typeof newId === "string") {
-        id = newId;
-      }
-    }
-
-    if (id) {
-      const result = await getMemberForClub(id, clubId);
-      console.log(result);
-      if (result) {
-        await putMember(clubId, id, { paid: data.paid, isAdmin: data.isAdmin });
-      } else {
-        await postMember({
-          club: clubId,
-          user: id,
-          paid: data.paid,
-          isAdmin: data.isAdmin,
+    let id = user?.id
+    if(!user) {            
+       const newId = await postUser(
+        {
+            name: data.name, 
+            email: data.email, 
+            upi: data.upi, 
+            year_of_study: data.year_of_study, 
+            student_id : data.student_id,
         });
-      }
+        if (typeof newId === 'string') {
+            id = newId
+        }
     }
-  });
+    
+    if(id) {
+        const result = await getMemberForClub(id , clubId)
+        console.log(result)
+        if(result) {
+            await putMember(clubId, id, {paid: data.paid, isAdmin: data.isAdmin})
+        }
+        else {
+            await postMember({club: clubId, user: id, paid: data.paid, isAdmin: data.isAdmin})
+        }
+    } 
+  }
 }
