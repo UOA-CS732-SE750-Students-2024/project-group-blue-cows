16eb417b6fb5afbf99ee7d38448783d84db4d4bb
TrissyG <tmon261@aucklanduni.ac.nz>
2024-05-08 01:05:46 +1200

    :sparkles: return to club page now redirects to relevant /view

--- src/app/clubs/[clubId]/signup/page.tsx
+++ src/app/clubs/[clubId]/signup/page.tsx
@@ -7,11 +7,12 @@ import ClubMembershipForm from "@/components/form/ClubMembershipForm";
 import { Button } from "@/components/ui/button";
 import { Club } from "@/schemas/clubSchema";
 import { getClubById } from "@/services/clubServices";
+import router, { useRouter } from "next/navigation";
 
 export default function Page() {
   const [clubData, setClubData] = useState<Club | null>(null);
   const params = useParams<{ clubId: string }>();
-
+  const router = useRouter();
   useEffect(() => {
     const getData = async () => {
       const clubData = await getClubById(Number(params.clubId));
@@ -25,7 +26,11 @@ export default function Page() {
     <section className="w-full">
       <div className="h-screen justify-center w-full pt-20 ">
         <div className="flex flex-row space-x-4  justify-center py-2">
-          <Button variant="destructive" className="min-w-max">
+          <Button
+            onClick={() => router.push(`/clubs/${params.clubId}/view`)}
+            variant="destructive"
+            className="min-w-max"
+          >
             <p> Return to Club Page </p>
           </Button>
           <img src={logo} alt="club logo" className="w-10 h-10" />
