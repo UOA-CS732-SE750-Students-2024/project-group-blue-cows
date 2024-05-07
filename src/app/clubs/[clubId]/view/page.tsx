3b1ae1280c5b1ab922dda3d9da759762e54d3d61
TrissyG <tmon261@aucklanduni.ac.nz>
2024-05-08 01:00:49 +1200

    :memo: register changed to signup

--- src/app/clubs/[clubId]/view/page.tsx
+++ src/app/clubs/[clubId]/view/page.tsx
@@ -75,7 +75,7 @@ export default function ClubViewPage({
                 onClick={navigateToSignup}
                 className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-150 ease-in-out"
               >
-                Register
+                Sign Up
               </button>
             </div>
           </div>
