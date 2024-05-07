275d1666f9cfb14fb3580a4c7268c1a727c24dc5
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-07 12:54:20 +1200

    test to fix bug

--- src/gateway/postMembersDataFromCsv.ts
+++ src/gateway/postMembersDataFromCsv.ts
@@ -6,27 +6,7 @@ import {postUser} from "./postUser";
 import { getMemberForClub } from "./getMemberForClub";
 
 export async function postMembersDataFromCSV(clubId: number, formData: FormData) {
-    //const studentData = await importCsvFile(formData);
-    const studentData = [
-        {
-          name: 'Lucy Zhu',
-          email: 'lzhu613@aucklanduni.ac.nz',
-          upi: 'abc',
-          year_of_study: 4,
-          student_id: 'abc',
-          paid: true,
-          isAdmin: false
-        },
-        {
-          name: 'Nate Williamson',
-          email: 'nwil508@aucklanduni.ac.nz',
-          upi: 'def',
-          year_of_study: 3,
-          student_id: 'def',
-          paid: false,
-          isAdmin: false
-        }
-    ]
+    const studentData = await importCsvFile(formData);
     for(const data of studentData) {
         const user = await getUserByEmail(data.email);
         let id = user?.id
