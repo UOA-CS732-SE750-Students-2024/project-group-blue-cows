e31305886cae9878ef1aaae4665093b9f7b9e1c4
vdav02 <100258989+vdav02@users.noreply.github.com>
2024-05-08 03:55:19 +1200

    Build fix
    Updated key, as the dto being used does not have an ID

--- src/components/misc/gallery.tsx
+++ src/components/misc/gallery.tsx
@@ -12,7 +12,7 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
     <div className="p-5 md:p-10">
       <div className="columns-1 gap-5 lg:gap-8 sm:columns-2 lg:columns-3 xl:columns-4 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
         {images.map((image) => (
-          <img key={image.id} src={image.imageUrl} alt={image.title} className="border-2 border-blue-700" />
+          <img key={image.imageUrl} src={image.imageUrl} alt={image.title} className="border-2 border-blue-700" />
         ))}
       </div>
     </div>
