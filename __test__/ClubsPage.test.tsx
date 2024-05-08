04861d67408450bf29deb40de3e2751483aec20b
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-08 15:16:44 +1200

    added tests to clubs page

--- __test__/ClubsPage.test.tsx
+++ __test__/ClubsPage.test.tsx
@@ -7,13 +7,32 @@ jest.mock("@/services/clubServices", () => ({
   getAllClubs: jest.fn(),
 }));
 
+const mockData = [
+  {
+    id: "test",
+    name: "WDCC",
+    description: "A club for cows",
+    membership_fee: "0.00",
+    category: "Academic and specialist",
+    logo: "test",
+  },
+  {
+    id: "meh",
+    name: "ASPA",
+    description: "A club for pool players",
+    membership_fee: "10.00",
+    category: "Sports",
+    logo: "test",
+  },
+];
+
 beforeEach(() => {
-  jest.resetAllMocks();
+  jest.clearAllMocks();
   //Arrange
   render(<ClubsPage />);
 });
 
-test("it renders input field on search clubs screen", () => {
+test("it renders input field", () => {
   // Act
   const inputFields = screen.getByPlaceholderText("Search");
 
@@ -21,7 +40,7 @@ test("it renders input field on search clubs screen", () => {
   expect(inputFields).toBeInTheDocument();
 });
 
-test("it renders all the filter buttons on search club screen", async () => {
+test("it renders all the filter buttons", async () => {
   const buttons = await screen.findAllByRole("button");
 
   const allButton = buttons.find((button) => button.textContent === "All");
@@ -56,18 +75,7 @@ test("it renders all the filter buttons on search club screen", async () => {
   expect(culturalButton).toBeInTheDocument();
 });
 
-test("it renders input field on search clubs screen and displays data from mock", async () => {
-  const mockData = [
-    {
-      id: "test",
-      name: "WDCC",
-      description: "A club for cows",
-      membership_fee: "0.00",
-      category: "Academic and specialist",
-      logo: "test",
-    },
-  ];
-
+test("it finds club based on input field", async () => {
   (getAllClubs as jest.Mock).mockResolvedValue(mockData);
   const inputField = screen.getByPlaceholderText("Search");
   fireEvent.change(inputField, { target: { value: "WDCC" } });
@@ -75,5 +83,18 @@ test("it renders input field on search clubs screen and displays data from mock"
   expect(inputField).toHaveValue("WDCC");
 
   const displayedData = await screen.findByText("A club for cows");
+  expect(displayedData).toBeInTheDocument();
+});
+
+test("it finds club based on category button", async () => {
+  (getAllClubs as jest.Mock).mockResolvedValue(mockData);
+
+  const buttons = await screen.findAllByRole("button");
+  const sportButton = buttons.find((button) => button.textContent === "Sport");
+  if (sportButton) {
+    fireEvent.click(sportButton);
+  }
+
+  const displayedData = await screen.findByText("A club for pool players");
   expect(displayedData).toBeInTheDocument();
 });
