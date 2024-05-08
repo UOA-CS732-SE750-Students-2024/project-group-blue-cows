f96725b621600fa247272194406c1cc8f5630f02
TrissyG <tmon261@aucklanduni.ac.nz>
2024-05-08 22:48:26 +1200

    :bug: fixed unintentional /clubs/new overwrite, and fixed compatibility for FormWrapper to be used in both club and member registration forms

--- src/components/form/form-header.tsx
+++ src/components/form/form-header.tsx
@@ -1,38 +1,43 @@
 import { Club } from "@/schemas/clubSchema";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useSearchParams } from "next/navigation";
 import { URLSearchParams } from "url";
 import { getClubById } from "@/services/clubServices";
 import LoadingSpinner from "../ui/loading-spinner";
+import { useRouter, useSearchParams } from "next/navigation";
+
 interface AuthHeaderProps {
   label: string;
   title: string | null;
-  params: { clubId: string }; // New prop
   formType: "registration" | "membership"; // New prop
+  clubId?: string;
 }
 
 export const FormHeader = ({
   label,
   title,
-  params,
   formType,
+  clubId,
 }: AuthHeaderProps) => {
   const [clubData, setClubData] = useState<Club | null>(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(
+    formType === "registration" ? false : true
+  );
+
   useEffect(() => {
     const getData = async () => {
-      const clubData = await getClubById(Number(params.clubId));
-      setClubData(clubData);
-      setLoading(false);
+      if (formType === "membership" && clubId) {
+        const clubData = await getClubById(Number(clubId));
+        setClubData(clubData);
+        setLoading(false);
+      }
     };
     getData();
-  }, []);
+  }, [formType, clubId]);
 
   if (loading) {
     return <LoadingSpinner />;
   }
-  console.log("Rendered FormHeader: ", clubData?.name);
+
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
       {formType === "registration" ? (
