ef6635a6cd105fb784e7e919d7a5d58f7b1c9a87
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-08 13:41:46 +1200

    test: added test to check if buttons render correctly

--- __test__/ClubsPage.test.tsx
+++ __test__/ClubsPage.test.tsx
@@ -2,13 +2,50 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ClubsPage from "@/app/clubs/page";
 
-test("it renders input field on search clubs screen", () => {
-  //Arrange
+beforeEach(() => {
+  // Arrange
   render(<ClubsPage />);
+});
 
+test("it renders input field on search clubs screen", () => {
   // Act
   const inputFields = screen.getByPlaceholderText("Search");
 
   // Assert
   expect(inputFields).toBeInTheDocument();
+});
+
+test("it renders all the filter buttons on search club screen", async () => {
+  const buttons = await screen.findAllByRole("button");
+
+  const allButton = buttons.find((button) => button.textContent === "All");
+  expect(allButton).toBeInTheDocument();
+
+  const academicButton = buttons.find(
+    (button) => button.textContent === "Academic and specialist"
+  );
+  expect(academicButton).toBeInTheDocument();
+
+  const sportButton = buttons.find((button) => button.textContent === "Sport");
+  expect(sportButton).toBeInTheDocument();
+
+  const specialButton = buttons.find(
+    (button) => button.textContent === "Special Interest"
+  );
+  expect(specialButton).toBeInTheDocument();
+
+  const religiousButton = buttons.find(
+    (button) => button.textContent === "Religious and spiritual"
+  );
+  expect(religiousButton).toBeInTheDocument();
+
+  const causesButton = buttons.find(
+    (button) => button.textContent === "Causes"
+  );
+  expect(causesButton).toBeInTheDocument();
+
+  const culturalButton = buttons.find(
+    (button) => button.textContent === "Cultural"
+  );
+  expect(culturalButton).toBeInTheDocument();
 });
