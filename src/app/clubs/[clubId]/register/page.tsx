ca8162d19602a6205599f54c100294a574e60974
TrissyG <tmon261@aucklanduni.ac.nz>
2024-05-08 19:25:16 +1200

    :bug: migrated /signup to /register

--- src/app/clubs/[clubId]/register/page.tsx
+++ src/app/clubs/[clubId]/register/page.tsx
@@ -1,5 +1,3 @@
-"use client"; // to get react to know it's a client compponent
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import FormWrapper from "@/components/form/form-wrapper";
@@ -7,36 +5,44 @@ import ClubMembershipForm from "@/components/form/ClubMembershipForm";
 import { Button } from "@/components/ui/button";
 import { Club } from "@/schemas/clubSchema";
 import { getClubById } from "@/services/clubServices";
+import router, { useRouter } from "next/navigation";
+import { getClubFormFields } from "@/gateway/clubFormField/getClubFormFields";
 
-export default function Page() {
-  const [clubData, setClubData] = useState<Club | null>(null);
-  const params = useParams<{ clubId: string }>();
+export default async function Page({
+  params: { clubId },
+}: {
+  params: { clubId: string };
+}) {
+  const club = await getClubById(+clubId);
+  const clubFormFields = await getClubFormFields(+clubId);
 
-  useEffect(() => {
-    const getData = async () => {
-      const clubData = await getClubById(Number(params.clubId));
-      setClubData(clubData);
-    };
-    getData();
-  }, []);
+  if (!club) return null;
+
+  const clubData = await getClubById(Number(+clubId));
 
   const logo = clubData?.logo || "";
   return (
     <section className="w-full">
       <div className="h-screen justify-center w-full pt-20 ">
         <div className="flex flex-row space-x-4  justify-center py-2">
-          <Button variant="destructive" className="min-w-max">
-            <p> Return to Club Page </p>
-          </Button>
+          <a href={`/clubs/${clubId}/view`}>
+            <Button variant="destructive" className="min-w-max">
+              <p> Return to Club Page </p>
+            </Button>
+          </a>
           <img src={logo} alt="club logo" className="w-10 h-10" />
         </div>
         <FormWrapper
           label="Membership Form"
           title=""
           formType="membership"
-          params={{ ...params }}
+          params={{ clubId: clubId }}
         >
-          <ClubMembershipForm params={{ ...params }} />
+          <ClubMembershipForm
+            clubId={clubId}
+            club={club}
+            clubFormFields={clubFormFields}
+          />
         </FormWrapper>
       </div>
     </section>
