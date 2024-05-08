95098147821e72190b450819a9db7b64a21bf07f
Luca Eastwood <77914022+leas022@users.noreply.github.com>
2024-05-08 14:07:48 +1200

    :bug: change edit profile path to /users/me

--- src/app/users/me/page.tsx
+++ src/app/users/me/page.tsx
@@ -1,7 +1,21 @@
-export default function ProfilePage() {
+import ProfileEditForm from "@/components/form/profile-edit-form";
+import { PageHeader } from "@/components/misc/PageHeader";
+import ProfileHeader from "@/components/profile/ProfileHeader";
+
+export default async function ProfilePage() {
   return (
-    <main>
-      <h1>Me!</h1>
+    <main className="h-[calc(100vh-4rem)] px-16 py-8">
+      <PageHeader
+        title="Edit Profile"
+        subtitle="Which clubs can see this data?"
+        href="/users/me/clubs"
+      />
+      <div className="mt-2">
+        <ProfileHeader />
+      </div>
+      <div className="my-2">
+        <ProfileEditForm />
+      </div>
     </main>
   );
 }
