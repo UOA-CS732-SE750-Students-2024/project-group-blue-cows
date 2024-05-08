b63e17c0a0af2744bc881bf95cfe3728edd9286c
Oculux <nate.williamson.nz@gmail.com>
2024-05-09 02:10:42 +1200

    :test_tube: add link tests

---
+++ e2e/links.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from "@playwright/test";
+
+test("browse and join clubs button links to /clubs", async ({ page }) => {
+  await page.goto("http://localhost:3000/users/me/clubs");
+  await page.getByRole("button", { name: "Browse and Join Clubs" }).click();
+  await expect(page).toHaveURL(/\/clubs/);
+});
+
+test("register a club button links to /clubs/new", async ({ page }) => {
+  await page.goto("http://localhost:3000/users/me/clubs");
+  await page.getByRole("button", { name: "Register a Club" }).click();
+  await expect(page).toHaveURL(/\/clubs\/new/);
+});
+
+test("browse clubs topnav button links to /clubs", async ({ page }) => {
+  await page.goto("http://localhost:3000/clubs");
+  await page.getByRole("link", { name: "Browse Clubs" }).click();
+  await expect(page).toHaveURL(/\/clubs/);
+});
+
+test("manage clubs topnav button links to /users/me/clubs", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/clubs");
+  await page.getByRole("link", { name: "Manage Clubs" }).click();
+  await expect(page).toHaveURL(/\/users\/me\/clubs/);
+});
+
+test("profile topnav button links to /users/me", async ({ page }) => {
+  await page.goto("http://localhost:3000/clubs");
+  await page.getByRole("link", { name: "Profile" }).click();
+  await expect(page).toHaveURL(/\/users\/me/);
+});
+
+test("search for clubs sidenav button links to /clubs", async ({ page }) => {
+  await page.goto("http://localhost:3000/clubs");
+  await page.getByRole("button", { name: "Search" }).click();
+  await expect(page).toHaveURL(/\/clubs/);
+});
