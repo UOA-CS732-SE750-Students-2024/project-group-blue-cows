395f2ad8bcf985296042c39dfb27a8677e81f6a7
Oculux <nate.williamson.nz@gmail.com>
2024-05-09 11:45:16 +1200

    :lipstick: fix spots image

--- src/components/layout/NavTop.tsx
+++ src/components/layout/NavTop.tsx
@@ -1,11 +1,6 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import Link from "next/link";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavTop({ className }: { className?: string }) {
   return (
@@ -13,31 +8,31 @@ export default function NavTop({ className }: { className?: string }) {
       className={`flex flex-row items-center w-full bg-blue-custom ${className}`}
     >
       <div>
-        <Link href="/">
+        <Link href="/" className="h-full">
           <Image
             src="/cowmunity-logo-title.svg"
             width={400}
             height={400}
             alt="Cowmunity Logo"
-            className="object-scale-down pt-2 pl-8"
+            className="pt-2 pl-8 h-full"
             priority
           />
         </Link>
       </div>
-      <div className="ml-20">
+      <div className="ml-20 h-full">
         <Image
-          src="/cowmunity-spots.svg"
+          src="/cowmunity-spots.png"
           width={500}
           height={400}
           alt="Cowmunity pattern for top navbar"
-          className="z-50"
+          className="z-50 h-full"
           priority
         />
       </div>
       <nav className="flex flex-row justify-end w-full space-x-4 items-center pl-2 pr-8 lg:py-10">
         <TooltipProvider>
           <NavItem href="/clubs" tooltip="Browse Clubs">
-            Browse Clubs
+            <div>Browse Clubs</div>
           </NavItem>
           <NavItem href="/users/me/clubs" tooltip="Manage Clubs">
             Manage Clubs
