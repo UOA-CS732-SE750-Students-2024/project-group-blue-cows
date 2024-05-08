4a24421eaf23c01d5bd86f1380c09d4a909f6f17
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-05-09 03:28:48 +1200

    refactor: remove test to test file

--- __test__/memberUtils.test.ts
+++ __test__/memberUtils.test.ts
@@ -1,6 +1,6 @@
 import { GetInputsForClubDto } from "@/Dtos/formFieldInput/GetInputsForClubDto";
 import { studentDataWithId } from "@/gateway/member/getAllMembersForClub";
-import { combineMembersData } from "@/util/memberUtil";
+import { combineMembersData, extractFieldNames } from "@/util/memberUtil";
 
 it("combines student data with form field inputs correctly", () => {
   const studentDataList: studentDataWithId[] = [
@@ -49,4 +49,48 @@ it("combines student data with form field inputs correctly", () => {
 
   // Assertion
   expect(combinedData).toEqual(expectedCombinedData);
+});
+
+it("returns an array of unique field names extracted from form field inputs", () => {
+  const inputsList: GetInputsForClubDto[] = [
+    {
+      userId: "user1",
+      formFieldInputs: [
+        { fieldName: "input1", value: "value1" },
+        { fieldName: "input2", value: "value2" },
+      ],
+    },
+  ];
+
+  const expectedFieldNames = ["input1", "input2"];
+
+  const fieldNames = extractFieldNames(inputsList);
+
+  expect(fieldNames).toEqual(expectedFieldNames);
+});
+
+it("returns an empty array when no form field inputs are provided", () => {
+  const inputsList: GetInputsForClubDto[] = [];
+
+  const expectedFieldNames: string[] = [];
+
+  const fieldNames = extractFieldNames(inputsList);
+
+  expect(fieldNames).toEqual(expectedFieldNames);
+});
+
+it("returns an empty array when form field inputs have duplicate field names", () => {
+  const inputsList: GetInputsForClubDto[] = [
+    {
+      userId: "user1",
+      formFieldInputs: [
+        { fieldName: "input1", value: "value1" },
+        { fieldName: "input1", value: "value2" },
+      ],
+    },
+  ];
+
+  const expectedFieldNames: string[] = ["input1"];
+  const fieldNames = extractFieldNames(inputsList);
+  expect(fieldNames).toEqual(expectedFieldNames);
 });
