ebf7dc88adb2d8c46f5a73733ca167f0dc9b86bd
Luca Eastwood <77914022+leas022@users.noreply.github.com>
2024-05-08 14:15:11 +1200

    :bug: ensure redirection if the user is logged out

--- src/app/users/me/page.tsx
+++ src/app/users/me/page.tsx
@@ -1,8 +1,18 @@
+"use server";
+
 import ProfileEditForm from "@/components/form/profile-edit-form";
 import { PageHeader } from "@/components/misc/PageHeader";
 import ProfileHeader from "@/components/profile/ProfileHeader";
+import { AppUser } from "@/schemas/authSchema";
+import { auth } from "@/util/auth";
+import { redirect } from "next/navigation";
 
 export default async function ProfilePage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/"); // redirect back to home page if user is not authenticated
+  }
   return (
     <main className="h-[calc(100vh-4rem)] px-16 py-8">
       <PageHeader
