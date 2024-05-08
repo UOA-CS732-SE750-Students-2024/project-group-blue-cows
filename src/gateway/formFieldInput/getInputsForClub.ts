e5d9d275f26e5ee3ac1a8e3ebb9c7e72220c856f
MRlolface249 <ahop089@aucklanduni.ac.nz>
2024-05-08 20:37:32 +1200

    fixed getInputsForClub so that it only returns data it's been authorised to see

--- src/gateway/formFieldInput/getInputsForClub.ts
+++ src/gateway/formFieldInput/getInputsForClub.ts
@@ -3,6 +3,10 @@ import { eq } from "drizzle-orm";
 import { GetInputsForClubDto } from "@/Dtos/formFieldInput/GetInputsForClubDto";
 import membershipSchema from "@/schemas/membershipSchema";
 import { getFormFieldInputs } from "./getFormFieldInputs";
+import formFieldInputSchema from "@/schemas/formFieldInputSchema";
+import userDataAuthorisedSchema from "@/schemas/userDataAuthorisedSchema";
+import FormFieldSchema from "@/schemas/FormFieldSchema";
+import { GetFormFieldInputDto } from "@/Dtos/formFieldInput/GetFormFieldInputDto";
 
 export async function getInputsForClub(
   clubId: number
@@ -13,10 +17,37 @@ export async function getInputsForClub(
     .where(eq(membershipSchema.club, clubId));
 
   const returns = users.map(async (user) => {
-    const formFieldInputs = await getFormFieldInputs(user.userId);
+    const formFieldInputs = await db
+      .select({
+        fieldName: FormFieldSchema.name,
+        value: formFieldInputSchema.value,
+      })
+      .from(formFieldInputSchema)
+      .leftJoin(
+        FormFieldSchema,
+        eq(formFieldInputSchema.formFieldId, FormFieldSchema.id)
+      )
+      .leftJoin(
+        userDataAuthorisedSchema,
+        eq(formFieldInputSchema.id, userDataAuthorisedSchema.formFieldInputId)
+      )
+      .leftJoin(
+        membershipSchema,
+        eq(userDataAuthorisedSchema.memberId, membershipSchema.id)
+      )
+      .where(eq(membershipSchema.club, clubId));
+    const formFieldInputDtos = formFieldInputs.map(
+      (formFieldInput): GetFormFieldInputDto => {
+        if (!formFieldInput.fieldName) throw new Error("name failed to return");
+        return {
+          fieldName: formFieldInput.fieldName,
+          value: formFieldInput.value,
+        };
+      }
+    );
     return {
       userId: user.userId,
-      formFieldInputs: formFieldInputs,
+      formFieldInputs: formFieldInputDtos,
     };
   });
   return Promise.all(returns);
