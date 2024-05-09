1cb7270918d80b9256043df46e541c7beb1b5a26
Lucy Zhu (lzhu613) <lzlucyz@gmail.com>
2024-05-09 15:04:25 +1200

    added more default fiedslf ro missing lcub info

--- src/components/misc/gallery.tsx
+++ src/components/misc/gallery.tsx
@@ -1,25 +1,27 @@
-"use client";
-
+import React from "react";
 import { PostGalleryImageDto } from "@/Dtos/image/PostGalleryImageDto";
 
 interface GalleryProps {
-  // images: typeof Image[];
   images: PostGalleryImageDto[];
 }
 
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
   return (
-    <div className="p-2 md:p-3">
-      <div className="columns-1 gap-5 lg:gap-8 sm:columns-2 lg:columns-3 xl:columns-4 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
-        {images.map((image) => (
-          <img
-            key={image.imageUrl}
-            src={image.imageUrl}
-            alt={image.title}
-            className="border-2 border-blue-700"
-          />
-        ))}
-      </div>
+    <div>
+      {images.length === 0 ? (
+        <p className="pt-1">No images provided</p>
+      ) : (
+        <div className="p-2 md:p-3 columns-1 gap-5 lg:gap-8 sm:columns-2 lg:columns-3 xl:columns-4 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
+          {images.map((image) => (
+            <img
+              key={image.imageUrl}
+              src={image.imageUrl}
+              alt={image.title}
+              className="border-2 border-blue-700"
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
